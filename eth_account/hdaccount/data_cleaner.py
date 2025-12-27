3c28d4c13f9c5982033716945eac2ebd5b88cfd6
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2025-12-27 15:22:09 +0000

    Add utility function to remove duplicate entries from a list

--- eth_account/hdaccount/data_cleaner.py
+++ eth_account/hdaccount/data_cleaner.py
@@ -86,3 +86,16 @@ def remove_outliers_iqr(data, column):
     
     filtered_data = data[(data[column] >= lower_bound) & (data[column] <= upper_bound)]
     return filtered_data
+def remove_duplicates(input_list):
+    seen = set()
+    result = []
+    for item in input_list:
+        if item not in seen:
+            seen.add(item)
+            result.append(item)
+    return result
+
+def clean_data(data):
+    if not isinstance(data, list):
+        raise TypeError("Input must be a list")
+    return remove_duplicates(data)
