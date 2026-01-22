86075cc8e2c16b4f5855f7a42bd45ecf57e29c1f
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-22 15:05:44 +0000

    Add utility function to remove duplicates from a list while preserving order

--- eth_account/typed_transactions/blob_transactions/data_cleaner.py
+++ eth_account/typed_transactions/blob_transactions/data_cleaner.py
@@ -6,3 +6,11 @@ def deduplicate_list(input_list):
             seen.add(item)
             result.append(item)
     return result
+def remove_duplicates(seq):
+    seen = set()
+    result = []
+    for item in seq:
+        if item not in seen:
+            seen.add(item)
+            result.append(item)
+    return result
