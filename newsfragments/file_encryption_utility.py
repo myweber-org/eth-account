67557cee4f4c854d10a619f933f3728e627906a6
ElaraWrites <elara.writes@outlook.com>
2026-02-04 16:47:49 +0000

    Add file encryption utility with AES-256-CBC mode

--- newsfragments/file_encryption_utility.py
+++ newsfragments/file_encryption_utility.py
@@ -187,4 +187,70 @@ def main():
         print("Invalid action")
 
 if __name__ == "__main__":
-    main()
+    main()import os
+import base64
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.backends import default_backend
+import hashlib
+
+def derive_key(password, salt):
+    return hashlib.pbkdf2_hmac('sha256', password.encode(), salt, 100000, 32)
+
+def encrypt_file(input_path, output_path, password):
+    salt = os.urandom(16)
+    key = derive_key(password, salt)
+    iv = os.urandom(16)
+    
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    
+    padder = padding.PKCS7(128).padder()
+    
+    with open(input_path, 'rb') as f_in, open(output_path, 'wb') as f_out:
+        f_out.write(salt + iv)
+        
+        while True:
+            chunk = f_in.read(4096)
+            if not chunk:
+                break
+            padded_data = padder.update(chunk)
+            encrypted_chunk = encryptor.update(padded_data)
+            f_out.write(encrypted_chunk)
+        
+        final_padded = padder.finalize()
+        final_encrypted = encryptor.update(final_padded) + encryptor.finalize()
+        f_out.write(final_encrypted)
+
+def decrypt_file(input_path, output_path, password):
+    with open(input_path, 'rb') as f_in:
+        salt = f_in.read(16)
+        iv = f_in.read(16)
+        key = derive_key(password, salt)
+        
+        cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+        decryptor = cipher.decryptor()
+        unpadder = padding.PKCS7(128).unpadder()
+        
+        with open(output_path, 'wb') as f_out:
+            while True:
+                chunk = f_in.read(4096)
+                if not chunk:
+                    break
+                decrypted_chunk = decryptor.update(chunk)
+                unpadded_data = unpadder.update(decrypted_chunk)
+                f_out.write(unpadded_data)
+            
+            final_decrypted = decryptor.finalize()
+            final_unpadded = unpadder.update(final_decrypted) + unpadder.finalize()
+            f_out.write(final_unpadded)
+
+def generate_checksum(file_path):
+    sha256_hash = hashlib.sha256()
+    with open(file_path, 'rb') as f:
+        for byte_block in iter(lambda: f.read(4096), b''):
+            sha256_hash.update(byte_block)
+    return sha256_hash.hexdigest()
+
+def validate_file_integrity(original_path, decrypted_path):
+    return generate_checksum(original_path) == generate_checksum(decrypted_path)
