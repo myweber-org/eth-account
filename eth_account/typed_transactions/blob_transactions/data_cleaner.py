4c69c5c150ffd4c7ca40ff011fd6a141bb2ce480
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-08 22:04:17 +0000

    Add data cleaning utility with outlier removal and normalization functions

--- eth_account/typed_transactions/blob_transactions/data_cleaner.py
+++ eth_account/typed_transactions/blob_transactions/data_cleaner.py
@@ -113,4 +113,50 @@ def clean_numeric_data(data, lower_bound=None, upper_bound=None):
             cleaned.append(num)
         except (ValueError, TypeError):
             continue
-    return cleaned
+    return cleanedimport numpy as np
+import pandas as pd
+
+def remove_outliers_iqr(dataframe, column):
+    Q1 = dataframe[column].quantile(0.25)
+    Q3 = dataframe[column].quantile(0.75)
+    IQR = Q3 - Q1
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+    return dataframe[(dataframe[column] >= lower_bound) & (dataframe[column] <= upper_bound)]
+
+def normalize_minmax(dataframe, column):
+    min_val = dataframe[column].min()
+    max_val = dataframe[column].max()
+    if max_val == min_val:
+        return dataframe[column].apply(lambda x: 0.0)
+    return dataframe[column].apply(lambda x: (x - min_val) / (max_val - min_val))
+
+def standardize_zscore(dataframe, column):
+    mean_val = dataframe[column].mean()
+    std_val = dataframe[column].std()
+    if std_val == 0:
+        return dataframe[column].apply(lambda x: 0.0)
+    return dataframe[column].apply(lambda x: (x - mean_val) / std_val)
+
+def clean_dataset(dataframe, numeric_columns):
+    cleaned_df = dataframe.copy()
+    for col in numeric_columns:
+        if col in cleaned_df.columns:
+            cleaned_df = remove_outliers_iqr(cleaned_df, col)
+            cleaned_df[col + '_normalized'] = normalize_minmax(cleaned_df, col)
+            cleaned_df[col + '_standardized'] = standardize_zscore(cleaned_df, col)
+    return cleaned_df
+
+def get_summary_statistics(dataframe, column):
+    if column not in dataframe.columns:
+        return {}
+    series = dataframe[column]
+    return {
+        'mean': series.mean(),
+        'median': series.median(),
+        'std': series.std(),
+        'min': series.min(),
+        'max': series.max(),
+        'count': series.count(),
+        'missing': series.isnull().sum()
+    }
